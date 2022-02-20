7661db999e0b7abed100d04a53c5085a09ac5597
汪光武 <wangguangwu@pukanghealth.com>
2022-02-20 18:03:19 +0800

    create a new project.

---
+++ cthulhu-craw/src/main/java/com/wangguangwu/CthulhuCrawApplication.java
@@ -0,0 +1,16 @@
+package com.wangguangwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangguangwu
+ */
+@SpringBootApplication
+public class CthulhuCrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CthulhuCrawApplication.class, args);
+    }
+
+}
---
+++ cthulhu-craw/src/test/java/com/wangguangwu/CthulhuCrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangguangwu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CthulhuCrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
