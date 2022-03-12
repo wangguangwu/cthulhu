7f7b44674ad57c56b6b88b3f1926cc9e68bbc390
汪光武 <wangguangwu@pukanghealth.com>
2022-03-12 11:48:45 +0800

    client process.

--- cthulhu-client/cthulhu-client-http/src/main/java/com/wangguangwu/client/entity/DataParse.java
+++ cthulhu-client/cthulhu-client-http/src/main/java/com/wangguangwu/client/entity/DataParse.java
@@ -7,4 +7,6 @@ public class DataParse {
 
     public static final String LI = "li";
 
+    public static final String JOB_LIST = "div[class=job-list]";
+
 }
--- cthulhu-client/cthulhu-client-http/src/main/java/com/wangguangwu/client/service/impl/WorkImpl.java
+++ cthulhu-client/cthulhu-client-http/src/main/java/com/wangguangwu/client/service/impl/WorkImpl.java
@@ -1,7 +1,9 @@
 package com.wangguangwu.client.service.impl;
 
 import cn.hutool.core.util.StrUtil;
+import com.wangguangwu.client.entity.DataParse;
 import com.wangguangwu.client.entity.Http;
+import com.wangguangwu.client.entity.Symbol;
 import com.wangguangwu.client.entity.ZhipinData;
 import com.wangguangwu.client.http.Response;
 import com.wangguangwu.client.service.Work;
@@ -9,6 +11,9 @@ import com.wangguangwu.client.utils.HtmlParse;
 import com.wangguangwu.client.utils.TokenUtil;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import javax.net.ssl.SSLSocketFactory;
 import java.io.*;
@@ -17,9 +22,12 @@ import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.wangguangwu.client.entity.Symbol.*;
+import static com.wangguangwu.client.utils.HtmlParse.formatHtml;
+import static com.wangguangwu.client.utils.StringUtil.findFirstIndexNumberOfStr;
 
 
 /**
@@ -113,7 +121,7 @@ public class WorkImpl implements Work {
      * @return list of salaryData
      */
     private List<ZhipinData> parseResponseBody(String responseBody) {
-        return HtmlParse.analysisData(responseBody);
+        return analysisData(responseBody);
     }
 
 
@@ -164,6 +172,88 @@ public class WorkImpl implements Work {
             log.error("Cthulhu client send http request error", e);
         }
         return null;
+    }
+
+    /**
+     * parse zhipin data
+     *
+     * @param html data from zhipin
+     * @return zhipinData
+     */
+    private List<ZhipinData> analysisData(String html) {
+        html = formatHtml(html);
+        Document doc = Jsoup.parse(html);
+
+        Elements rows = new Elements();
+
+        if (doc.select(DataParse.JOB_LIST).size() > 0) {
+            rows = doc.select("div[class=job-list]").get(0).select("ul");
+        }
+
+        List<ZhipinData> list = new ArrayList<>();
+
+        if (rows.size() == 0) {
+            log.info("no results");
+        } else {
+            for (int i = 0; i < rows.get(0).select(DataParse.LI).size(); i++) {
+                ZhipinData data = new ZhipinData();
+
+                Elements jobPrimary = rows.get(0).select("li").get(i).select("[class=job-primary]");
+                // basic part
+                Elements infoPrimary = jobPrimary.select("[class=info-primary]");
+                Elements infoAppend = jobPrimary.select("[class=info-append clearfix]");
+
+                // primary-box
+                Elements primaryBox = infoPrimary.select("[class=primary-wrapper]")
+                        .select("[class=primary-box]");
+                // company-text
+                Elements companyText = infoPrimary.select("[class=info-company]")
+                        .select("[class=company-text]");
+
+                data.setCompanyName(companyText.select("h3").text());
+
+                final Elements p = companyText.select("p");
+                final String companyInfo = p.text();
+                // companyIndustry
+                final String companyIndustry = p.select("a").text();
+
+                data.setCompanyIndustry(companyIndustry);
+                String companyStatus = companyInfo.substring(companyIndustry.length());
+
+                int statusIndex = findFirstIndexNumberOfStr(companyStatus);
+
+                if (statusIndex > 0) {
+                    data.setCompanySize(companyStatus.substring(statusIndex));
+                    companyStatus = companyStatus.substring(0, statusIndex);
+                }
+                data.setCompanyStatus(companyStatus);
+
+                // jobTitle
+                String jobTitle = primaryBox.select("[class=job-title]").text();
+                int index = jobTitle.lastIndexOf(Symbol.SPACE);
+                if (index != -1) {
+                    data.setJobName(jobTitle.substring(0, index));
+                    data.setCompanyAddress(jobTitle.substring(index + 1));
+                }
+
+                // job-limit clearfix
+                String jobLimitClearfix = primaryBox.select("[class=job-limit clearfix]").text();
+                String[] strings = jobLimitClearfix.split(Symbol.SPACE);
+                data.setJobSalary(strings[0]);
+                data.setJobEducationRequire(strings[1]);
+
+                // tags
+                data.setJobSkillRequire(infoAppend.select("[class=tags]").text());
+                // infoDesc
+                data.setJobInfoDesc(infoAppend.select("[class=info-desc]").text());
+
+                list.add(data);
+                System.out.println(data);
+
+            }
+        }
+
+        return list;
     }
 
 }
